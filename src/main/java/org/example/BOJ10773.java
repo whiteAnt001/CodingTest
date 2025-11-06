26eb0900be3923ab1708bf58dfe38cb084582681
whiteAnt001 <westjun01@gmail.com>
2025-11-06 18:04:54 +0900

    2025-11-06

--- src/main/java/org/example/BOJ10773.java
+++ src/main/java/org/example/BOJ10773.java
@@ -1,10 +1,9 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.Stack;
 
-import java.io.*;
-import java.util.*;
-
-public class Main {
+public class BOJ10773 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
