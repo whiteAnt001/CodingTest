7e3ef301b1bcbede460a1c642971f00fcea9bb53
whiteAnt001 <westjun01@gmail.com>
2026-02-09 15:08:46 +0900

    2026-02-09

---
+++ src/main/java/org/example/BOJ1065.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class BOJ1065 {
+
+}
--- src/main/java/org/example/BOJ15989.java
+++ src/main/java/org/example/BOJ15989.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class BOJ15989 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
